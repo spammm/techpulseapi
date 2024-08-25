c32c4ef52814825a372c465ff37b390010cf73c1
k911 <k911@inbox.ru>
2024-08-25 22:08:48 +0300

    update cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
   app.enableCors({
     exposedHeaders: ['X-Total-Count'],
     origin: [process.env.CORS_CLIENT_URL, process.env.CORS_ADMIN_URL],
@@ -12,9 +13,25 @@ async function bootstrap() {
     allowedHeaders: 'Content-Type, Authorization',
     credentials: true,
   });
+
+  app.use((req, res, next) => {
+    if (req.method === 'OPTIONS') {
+      res.header(
+        'Access-Control-Allow-Methods',
+        'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+      );
+      res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+      res.header('Access-Control-Allow-Origin', process.env.CORS_CLIENT_URL);
+      res.sendStatus(204);
+    } else {
+      next();
+    }
+  });
+
   app.useStaticAssets(join(__dirname, '..', '..', 'uploads'), {
     prefix: '/uploads/',
   });
+
   await app.listen(9000);
 }
 bootstrap();
